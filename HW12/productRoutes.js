d8ebbb9aea546bae7ee195de8ccb6419a2733ec3
Longnguvl <long.nguyen220201@vnuk.edu.vn>
2024-11-21 19:35:17 +0700

    Delete HW12 directory

--- HW12/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/productController');
-
-// Routes for products (only showing data)
-router.get('/', productController.getAllProducts);           // Get all products
-router.get('/:id', productController.getProductById);       // Get a single product by ID
-
-module.exports = router;
